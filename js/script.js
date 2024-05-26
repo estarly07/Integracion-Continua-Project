87ce342aadbabdc8e0f15f235ced6f9efe530207
estarly.amaya <estarly.amaya@servinformacion.com>
2024-05-26 14:06:08 -0500

    CREACION
    - Se creo una vista para mostrar una receta

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     loadComponent('header', 'views/home/header_home.html');
     loadComponent('main', 'views/home/main_home.html');
+    loadComponent('recipe', 'views/home/recipe_home.html');
 });
 
 function loadComponent(id, url) {
@@ -10,4 +11,11 @@ function loadComponent(id, url) {
             document.getElementById(id).innerHTML = data;
         })
         .catch(error => console.error('Error cargando el componente:', error));
+}
+function showRecipeModal() {
+    document.getElementById('recipeModal').style.display = 'block';
+}
+
+function closeRecipeModal() {
+    document.getElementById('recipeModal').style.display = 'none';
 }
