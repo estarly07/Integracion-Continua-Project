35a2aba114f147de40097ab8f479357b67c39689
estarly.amaya <estarly.amaya@servinformacion.com>
2024-05-26 12:56:12 -0500

    CREACION
    - se creo el header de la pagina donde se muestra un buscador, el nombre
      de la pagina e iconos de las redes sociales

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    loadComponent('header', 'views/home/header_home.html');
+});
+
+function loadComponent(id, url) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(id).innerHTML = data;
+        })
+        .catch(error => console.error('Error cargando el componente:', error));
+}
