ea45c1667a3d829a61714af18a6be82d1fb6d359
fucalex <fuentesnet06@hotmail.com>
2024-05-27 19:01:26 -0500

    Buscador
    Se agrego una funcion de búsqueda en el .js, a partir de la barra de búsqueda, la cual desplegará una caja con resultados en el header_home.html. El estilo correspondiente fue agregado en styles.css.
    Adicionalmente se agregó una receta.

--- js/script.js
+++ js/script.js
@@ -19,3 +19,35 @@ function showRecipeModal() {
 function closeRecipeModal() {
     document.getElementById('recipeModal').style.display = 'none';
 }
+//Adicionar redireccionamientos a las recetas de acuerdo a como se vaya llenando el contenido
+let data = [
+    "Pizza napolitana",
+    "Hamburguesa al carbon"
+]
+
+function search() {
+    let query = document.getElementById("search-input").value;
+    console.log(query);
+    if (query.trim() === "") {
+        return;
+    }
+    let results = [];
+    for(let i = 0; i < data.length; i++){
+        if (data[i].toLowerCase().includes(query.toLowerCase())){
+            results.push(data[i]);
+            }
+        }
+    document.getElementById("results").innerHTML = "";
+    if(results.length > 0){
+        for(let i = 0; i<results.length; i++){
+            let li = document.createElement("li");
+            li.textContent = results[i];
+            document.getElementById("results").appendChild(li);
+        }
+    }
+    else{
+        let li = document.createElement("li");
+        li.textContent = "No se encoontraron recetas para: " + query,
+        document.getElementById("results").appendChild(li);
+    }        
+}
